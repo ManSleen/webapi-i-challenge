e67da073d5031e6453b6e9500d4147cf0241d736
Mike VanSleen <mike.vansleen@gmail.com>
2019-08-05 13:57:44 -0600

    First GET request complete

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // implement your API here
+const express = require("express");
+
+const Hubs = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("hello world, its Mike!");
+});
+
+server.get("/api/users", (req, res) => {
+  Hubs.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error getting the list of users" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const hubId = req.params.id;
+  Hubs.find()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error getting user by ID" });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  const hubInfo = req.body;
+  console.log("hub info from body!: ", hubInfo);
+  Hubs.insert(hubInfo)
+    .then(result => {
+      res.status(201).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error adding the hub" });
+    });
+});
+
+server.delete("hubs/:id", (req, res) => {
+  const hubId = req.params.id;
+
+  Hubs.remove(hubId)
+    .then(hub => {
+      res.status(200).json({ message: "hub got deleted!" });
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error removing hub" });
+    });
+});
+
+const port = 8001;
+
+server.listen(port, () => {
+  console.log(`Running on port ${port} captain!`);
+});
